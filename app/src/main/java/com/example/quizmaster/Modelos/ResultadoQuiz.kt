0e6cc9f827934a4928eb7cb0bf40f42ee64f45b5
Gaby1810 <gabriela.figueroa.jst.5@gmail.com>
2026-09-05 14:11:23 -0600

    Creacion del proyecto

---
+++ app/src/main/java/com/example/quizmaster/Modelos/ResultadoQuiz.kt
@@ -0,0 +1,21 @@
+package com.example.quizmaster.Modelos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.quizmaster.R
+
+class ResultadoQuiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_resultado_quiz)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
